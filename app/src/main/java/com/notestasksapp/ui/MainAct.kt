1a33b0f3b9ab4aaaf15831bd410469b5dfcf9161
Saoudi Abderraouf <ha_saoudi@esi.dz>
2024-07-10 02:49:13 +0100

    adding main activity

--- app/src/main/java/com/notestasksapp/ui/MainAct.kt
+++ app/src/main/java/com/notestasksapp/ui/MainAct.kt
@@ -3,10 +3,54 @@ package com.notestasksapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.notestasksapp.R
+import com.notestasksapp.ui.frags.NotesFrag
+import com.notestasksapp.ui.frags.ProfileFrag
+import com.notestasksapp.ui.frags.TasksFrag
+import kotlinx.android.synthetic.main.act_main.*
 
 class MainAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
+
+        initialization()
+
+        tasks_btn.setOnClickListener {
+            tasks_img.setImageResource(R.drawable.tasks_in)
+            notes_img.setImageResource(R.drawable.notes_out)
+            profile_img.setImageResource(R.drawable.profile_out)
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TasksFrag()).commit()
+        }
+
+        notes_btn.setOnClickListener {
+            tasks_img.setImageResource(R.drawable.tasks_out)
+            notes_img.setImageResource(R.drawable.notes_in)
+            profile_img.setImageResource(R.drawable.profile_out)
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, NotesFrag()).commit()
+        }
+
+        profile_btn.setOnClickListener {
+            tasks_img.setImageResource(R.drawable.tasks_out)
+            notes_img.setImageResource(R.drawable.notes_out)
+            profile_img.setImageResource(R.drawable.profile_in)
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ProfileFrag()).commit()
+        }
+
     }
+
+    private fun initialization() {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TasksFrag()).commit()
+    }
+
+    override fun onBackPressed() {
+        tasks_img.setImageResource(R.drawable.tasks_in)
+        notes_img.setImageResource(R.drawable.notes_out)
+        profile_img.setImageResource(R.drawable.profile_out)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TasksFrag()).commit()
+    }
+
 }
