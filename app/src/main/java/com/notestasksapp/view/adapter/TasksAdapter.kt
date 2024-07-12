881822c51b284e648ca8d8e1c5b476307b186636
Saoudi Abderraouf <ha_saoudi@esi.dz>
2024-07-12 15:14:31 +0100

    adding search task activity

---
+++ app/src/main/java/com/notestasksapp/view/adapter/TasksAdapter.kt
@@ -0,0 +1,124 @@
+package com.notestasksapp.view.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import com.notestasksapp.R
+import com.notestasksapp.database.DBHandler
+import com.notestasksapp.helper.Helper
+import com.notestasksapp.model.Task
+import com.notestasksapp.model.TodoItem
+import com.notestasksapp.view.ui.tasks.PriorityTaskDetailsAct
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
+
+class TasksAdapter(val context: Context, var data: ArrayList<Task>, var dbHandler: DBHandler) :
+    RecyclerView.Adapter<TasksAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(R.layout._row_task, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val pos = position
+        val todoItems = dbHandler.todos.filter { todoItem -> todoItem.task == data[pos].id } as ArrayList<TodoItem>
+        var progress = 0
+        progress = if (data[pos].finished == 1) 100
+        else 0
+        if (data[pos].category == "Priority task"){
+            val allItems = todoItems.size
+            val finishedItems = (todoItems.filter { todoItem -> todoItem.finished == 1 } as ArrayList<TodoItem>).size
+            progress = (100 * finishedItems / allItems)
+        }
+        val calendar = Calendar.getInstance()
+        val day = data[pos].end_date.substringBefore(".").toInt()
+        val month = data[pos].end_date.substringBeforeLast(".").substringAfter(".").toInt()
+        val year = data[pos].end_date.substringAfterLast(".").toInt()
+        val endDate = Calendar.getInstance()
+        endDate.set(year, month - 1, day, 23, 59, 59)
+
+        holder.task_title_txt.text = data[pos].title
+        holder.task_description_txt.text = data[pos].description
+        holder.task_remaining_time_txt.text = "x days"
+        holder.task_progress_txt.text = "$progress%"
+        holder.task_progress.progress = progress
+
+        if (calendar.timeInMillis > endDate.timeInMillis){
+            holder.task_remaining_time_txt.text = "end"
+        }else{
+            holder.task_remaining_time_txt.text = getRemainingTime(endDate.timeInMillis)
+        }
+
+        val cardView = holder.itemView as CardView
+        if (pos % 4 == 0){
+            cardView.setCardBackgroundColor(Color.parseColor("#006EE9"))
+        }else if (pos % 4 == 1){
+            cardView.setCardBackgroundColor(Color.parseColor("#311F65"))
+        }else if (pos % 4 == 2){
+            cardView.setCardBackgroundColor(Color.parseColor("#D92C2C"))
+        }else{
+            cardView.setCardBackgroundColor(Color.parseColor("#1D6820"))
+        }
+
+        holder.itemView.setOnClickListener {
+            Helper.selected_task = data[pos]
+            context.startActivity(Intent(context, PriorityTaskDetailsAct::class.java))
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val task_title_txt = itemView.findViewById<TextView>(R.id.task_title_txt)
+        val task_description_txt = itemView.findViewById<TextView>(R.id.task_description_txt)
+        val task_remaining_time_txt = itemView.findViewById<TextView>(R.id.task_remaining_time_txt)
+        val task_progress_txt = itemView.findViewById<TextView>(R.id.task_progress_txt)
+        val task_progress = itemView.findViewById<LinearProgressIndicator>(R.id.task_progress)
+    }
+
+    private fun getRemainingTime(targetTimeInMillis: Long): String {
+        val currentTimeInMillis = System.currentTimeMillis()
+        val diffInMillis = targetTimeInMillis - currentTimeInMillis
+
+        // Convert milliseconds to days, hours, and minutes
+        val totalDays = TimeUnit.MILLISECONDS.toDays(diffInMillis)
+        val totalHours = TimeUnit.MILLISECONDS.toHours(diffInMillis)
+        val totalMinutes = TimeUnit.MILLISECONDS.toMinutes(diffInMillis)
+
+        // Calculate remaining months, days, and hours
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = currentTimeInMillis
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        calendar.timeInMillis = targetTimeInMillis
+        val targetMonth = calendar.get(Calendar.MONTH)
+        val targetDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val targetHour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        val months = targetMonth - currentMonth + (if (targetDay >= currentDay) 0 else -1)
+        val days = targetDay - currentDay + (if (targetHour >= currentHour) 0 else -1)
+        val hours = (totalHours % 24).toInt()
+
+        if (months > 0) return "$months months"
+        else if (days > 0) return "$days days"
+        else return "$hours hours"
+    }
+
+}
+
--- app/src/main/java/com/notestasksapp/view/ui/tasks/SearchTaskAct.kt
+++ app/src/main/java/com/notestasksapp/view/ui/tasks/SearchTaskAct.kt
@@ -2,11 +2,71 @@ package com.notestasksapp.view.ui.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.recyclerview.widget.GridLayoutManager
 import com.notestasksapp.R
+import com.notestasksapp.database.DBHandler
+import com.notestasksapp.model.Task
+import com.notestasksapp.view.adapter.TasksAdapter
+import kotlinx.android.synthetic.main.act_search_task.*
 
 class SearchTaskAct : AppCompatActivity() {
+
+    private lateinit var dbHandler: DBHandler
+    private lateinit var tasks: ArrayList<Task>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_search_task)
+
+        initialization()
+
+        search_edt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (search_edt.text.isNullOrEmpty()){
+                    tasks_list.adapter = TasksAdapter(this@SearchTaskAct, tasks, dbHandler)
+                }else{
+                    tasks_list.adapter = TasksAdapter(this@SearchTaskAct, ArrayList(), dbHandler)
+
+                    val filteredTasks = ArrayList<Task>()
+                    filteredTasks.addAll(tasks.filter { task -> task.title.toLowerCase().contains(search_edt.text.toString().toLowerCase())
+                            || task.description.toLowerCase().contains(search_edt.text.toString().toLowerCase()) })
+
+                    tasks_list.adapter = TasksAdapter(this@SearchTaskAct, filteredTasks, dbHandler)
+                }
+            }
+        })
+
+        clear_btn.setOnClickListener {
+            search_edt.setText("")
+            tasks_list.adapter = TasksAdapter(this@SearchTaskAct, tasks, dbHandler)
+        }
+
+        cancel_btn.setOnClickListener {
+            onBackPressed()
+        }
+
     }
+
+    private fun initialization() {
+        dbHandler = DBHandler(this)
+
+        tasks_list.layoutManager = GridLayoutManager(this, 2)
+        tasks_list.setHasFixedSize(true)
+
+        tasks_list.adapter = TasksAdapter(this, ArrayList(), dbHandler)
+
+        tasks = ArrayList()
+        tasks.clear()
+        tasks = dbHandler.tasks
+
+        tasks_list.adapter = TasksAdapter(this, tasks, dbHandler)
+
+    }
+
 }
